ee6cca19bf70255b230555a47a53a701fa2d2006
Girishma13 <girishmagiri13@gmail.com>
2025-12-18 11:01:05 +0530

    Update Main.java

--- data/src/Main.java
+++ data/src/Main.java
@@ -1,1 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            File file = new File("data/sample1.java");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+        } catch (Exception e) {
+            System.out.println("Error reading file");
+        }
+    }
+}
 
